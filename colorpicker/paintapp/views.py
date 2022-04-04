ae5a6d0bf708dce3e81894313a943f2b96ab6131
sasianan19 <sasianan19@gmail.com>
2022-04-03 21:12:34 -0600

    Hopefully adding the form elements to the html page shows up?

--- colorpicker/paintapp/views.py
+++ colorpicker/paintapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+
+from paintapp.forms import ColorPickerForm
 
 # Create your views here.
+class ColorPickerView(View):
+    def get(self, request):
+        form = ColorPickerForm()
+
+        context = {
+            'form': form,
+            'red': 200,
+            'green': 10,
+            'blue': 50,
+        }
+
+        return render(request, 'paint.html', context=context)
